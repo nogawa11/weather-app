d3ca904edce8b1af8d28d091c2e0417909b059ea
Nicole Ogawa <nogawa11@gmail.com>
2022-05-07 11:15:15 +0900

    added icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Weather = () => {
   const [city, setCity] = React.useState();
@@ -9,6 +11,10 @@ const Weather = () => {
 
   return (
     <div className="card--weather">
+      <button className="btn--mode">
+        <span className="icon--mode"><FontAwesomeIcon icon = {faMoon} /></span>
+        " Dark Mode"
+      </button>
       <h1>Weather in</h1>
       <form>
         <input name="city" type="text" value={city ? city : ''} onChange={handleFilter} />
